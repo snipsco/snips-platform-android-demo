a3d483e7faf0b9e0f653cf8d741669181252a91f
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2018-03-28 16:49:32 +0200

    update to 0.53.17

--- src/main/java/ai/snips/snipsdemo/DemoApplication.java
+++ src/main/java/ai/snips/snipsdemo/DemoApplication.java
@@ -14,11 +14,11 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // Snips launches a second process to run the platform, this allows to completely free the memory used by snips
-        // when not using it (the .so are quite huge, and you can't unload them from a process on android) and it
+        // Snips launches a second process to run the platform, this allows to completely free the memory used by Snips
+        // when not using it (the .so are quite huge, and you can't unload them from a process on Android) and it
         // isolates you app from potential crashes of the platform (we strive no to crash but this can happen, and the
-        // OS is less forgiving with a segfault than with an uncaught exception...
-        // The application is instantiated in both the man process and the snips one. If you need to initialize things
+        // OS is less forgiving with a segfault in native code than with an uncaught exception in Java... )
+        // The application is instantiated in both the main process and the snips one. If you need to initialize things
         // here, check you're not in the snips process
         if (!isSnipsProcess()) {
             Log.i("SnipsDemoApp", "in the main process");
